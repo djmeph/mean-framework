2e4dcb6a30381ec14a57948a448fa2abbcde919d
Norman Witte III <norm.witte@gmail.com>
2017-08-13 15:20:46 -0400

    add tracking for root redirect

--- routes/link.js
+++ routes/link.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.json');
 
 var redirect = require('../libs/redirect');
+var Hit = require('../services/hit');
 
 /* Lookup URL redirect and forward */
 router.get('/:slug', redirect, function (req, res) {
@@ -11,7 +13,15 @@ router.get('/:slug', redirect, function (req, res) {
 
 /* Forward root to djmeph.net  */
 router.get('/', function (req, res) {
+
+  Hit.post({
+    address: req.connection.remoteAddress,
+    useragent: req.headers['user-agent'],
+    Link: config.djmeph_id
+  });
+
   res.redirect(302, 'http://djmeph.net/');
+
 });
 
 
